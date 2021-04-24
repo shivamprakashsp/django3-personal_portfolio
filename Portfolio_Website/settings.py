ad6448716b557a27b6d2191ee46b4dc12748f603
Shivam <shivamprakashsp@gmail.com>
2021-04-24 07:47:45 +0000

    Removed old stuff

--- Portfolio_Website/settings.py
+++ Portfolio_Website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4pj&itx62vcq1+v)=r#c@m*kc3k=!4+2%=tes9psfic-3h!=*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shivamprakashsp.pythonanywhere.com']
 
 
 # Application definition
